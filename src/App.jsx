d0b2524703c6d42d74814969318b7ef4c5a334bc
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-24 22:38:05 +0900

    [App.jsx] 라우터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Selling from "./pages/Selling";
+import AddProducts from "./components/AddProducts";
 
 function App() {
-  return <div>FIRST</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sell" element={<Selling />} />
+        <Route path="/sell/add" element={<AddProducts />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
