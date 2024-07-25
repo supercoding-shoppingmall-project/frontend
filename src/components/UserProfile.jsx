81ed4b851a072466099efd8b73cdd60c1b5777f4
aa1as <sm13929881@gmail.com>
2024-07-25 15:57:08 +0900

    [UserProfile.jsx] 프로필 이미지 사진추가 구현

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 
 const UserProfile = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="col-span-full">
+        <label
+          htmlFor="photo"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          프로필 사진
+        </label>
+        <div className="mt-2 flex items-center gap-x-3">
+          <UserCircleIcon
+            aria-hidden="true"
+            className="h-12 w-12 text-gray-300"
+          />
+          <button
+            type="button"
+            className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          >
+            변경
+          </button>
+        </div>
+      </div>
+      <div className="col-span-full">
+        <label
+          htmlFor="cover-photo"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          파일 찾기
+        </label>
+        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+          <div className="text-center">
+            <PhotoIcon
+              aria-hidden="true"
+              className="mx-auto h-12 w-12 text-gray-300"
+            />
+            <div className="mt-4 flex text-sm leading-6 text-gray-600">
+              <label
+                htmlFor="file-upload"
+                className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+              >
+                <span>Upload a file</span>
+                <input
+                  id="file-upload"
+                  name="file-upload"
+                  type="file"
+                  className="sr-only"
+                />
+              </label>
+              <p className="pl-1">or drag and drop</p>
+            </div>
+            <p className="text-xs leading-5 text-gray-600">
+              PNG, JPG, GIF up to 10MB
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UserProfile;
