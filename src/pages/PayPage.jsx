6769a4f98b46b76a3ef9a969be71fa5771607c58
hbin3673 <hbin3673@gmail.com>
2024-07-26 00:14:58 +0900

    [PayPage] 결제 페이지에 장바구니 리스트 구현 및 결제 정보 추가

--- src/pages/PayPage.jsx
+++ src/pages/PayPage.jsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useState } from "react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import { Field, Label, Switch } from "@headlessui/react";
+import CartPage from "./CartPage";
 
 export default function Example() {
-  const [agreed, setAgreed] = useState(false);
+  const [paymentMethod, setPaymentMethod] = useState("card");
+
+  const handlePaymentMethodChange = (method) => {
+    setPaymentMethod(method);
+  };
 
   return (
     <div className="isolate bg-white px-6">
@@ -23,7 +26,7 @@ export default function Example() {
       </div>
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight t-0 text-gray-900 sm:text-4xl">
-          주문/결제
+          주문 / 결제
         </h2>
       </div>
       <form
@@ -45,7 +48,24 @@ export default function Example() {
                 name="name"
                 type="text"
                 autoComplete="given-name"
-                className="block w-1/3 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-24 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="phone-number"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              전화번호
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="phone-number"
+                name="phone-number"
+                type="text"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -54,116 +74,129 @@ export default function Example() {
               htmlFor="address"
               className="block text-sm font-semibold leading-6 text-gray-900"
             >
-              배송지
+              배송지{"   "}
+              <span className="text-xs leading-6 text-gray-500">
+                {"   "}도로명 주소 / 상세 주소 / 우편 번호
+              </span>
             </label>
             <div className="mt-2.5">
               <input
                 id="address"
                 name="address"
-                type="text"
-                autoComplete="organization"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Email
-            </label>
-            <div className="mt-2.5">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="phone-number"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Phone number
-            </label>
-            <div className="relative mt-2.5">
-              <div className="absolute inset-y-0 left-0 flex items-center">
-                <label htmlFor="country" className="sr-only">
-                  Country
-                </label>
-                <select
-                  id="country"
-                  name="country"
-                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-                >
-                  <option>US</option>
-                  <option>CA</option>
-                  <option>EU</option>
-                </select>
-                <ChevronDownIcon
-                  aria-hidden="true"
-                  className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                />
-              </div>
-              <input
-                id="phone-number"
-                name="phone-number"
                 type="tel"
                 autoComplete="tel"
-                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Message
-            </label>
-            <div className="mt-2.5">
-              <textarea
-                id="message"
-                name="message"
-                rows={4}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                defaultValue={""}
               />
             </div>
           </div>
-          <Field className="flex gap-x-4 sm:col-span-2">
-            <div className="flex h-6 items-center">
-              <Switch
-                checked={agreed}
-                onChange={setAgreed}
-                className="group flex w-8 flex-none cursor-pointer rounded-full bg-gray-200 p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 data-[checked]:bg-indigo-600"
-              >
-                <span className="sr-only">Agree to policies</span>
-                <span
-                  aria-hidden="true"
-                  className="h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out group-data-[checked]:translate-x-3.5"
-                />
-              </Switch>
-            </div>
-            <Label className="text-sm leading-6 text-gray-600">
-              By selecting this, you agree to our{" "}
-              <a href="#" className="font-semibold text-indigo-600">
-                privacy&nbsp;policy
-              </a>
-              .
-            </Label>
-          </Field>
         </div>
-        <div className="mt-10">
+
+        <div className="max-w-lg mx-auto p-4">
+          <h2 className="text-2xl font-bold mb-4">
+            결제 정보 (Payment Information)
+          </h2>
+
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-medium text-gray-700">
+              결제 수단
+            </label>
+            <div className="flex space-x-4">
+              <button
+                type="button"
+                onClick={() => handlePaymentMethodChange("card")}
+                className={`py-2 px-4 border rounded-md ${
+                  paymentMethod === "card"
+                    ? "bg-blue-500 text-white"
+                    : "bg-white text-gray-700"
+                }`}
+              >
+                신용/직불 카드
+              </button>
+              <button
+                type="button"
+                onClick={() => handlePaymentMethodChange("bank")}
+                className={`py-2 px-4 border rounded-md ${
+                  paymentMethod === "bank"
+                    ? "bg-blue-500 text-white"
+                    : "bg-white text-gray-700"
+                }`}
+              >
+                계좌이체
+              </button>
+            </div>
+          </div>
+
+          {paymentMethod === "card" && (
+            <div>
+              <div className="mb-4">
+                <label className="block mb-2 text-sm font-medium text-gray-700">
+                  카드 번호
+                </label>
+                <input
+                  type="text"
+                  className="w-full p-2 border rounded-md"
+                  placeholder="1234 5678 9012 3456"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block mb-2 text-sm font-medium text-gray-700">
+                  만료일
+                </label>
+                <input
+                  type="text"
+                  className="w-1/3 p-2 border rounded-md"
+                  placeholder="MM/YY"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block mb-2 text-sm font-medium text-gray-700">
+                  CVV 코드
+                </label>
+                <input
+                  type="password"
+                  className="w-1/3 p-2 border rounded-md"
+                  placeholder="123"
+                />
+              </div>
+            </div>
+          )}
+
+          {paymentMethod === "bank" && (
+            <div>
+              <div className="mb-4">
+                <label className="block mb-2 text-sm font-medium text-gray-700">
+                  은행 이름
+                </label>
+                <input
+                  type="text"
+                  className="w-full p-2 border rounded-md"
+                  placeholder="은행 이름"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block mb-2 text-sm font-medium text-gray-700">
+                  계좌 번호
+                </label>
+                <input
+                  type="text"
+                  className="w-full p-2 border rounded-md"
+                  placeholder="계좌 번호"
+                />
+              </div>
+            </div>
+          )}
+          <CartPage showPurchaseButton={false} />
+        </div>
+
+        <div className="mt-10 flex justify-center">
           <button
             type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+            className="w-1/4 rounded-md mb-10 bg-blue-600 px-3.5 py-2.5 text-center text-lg font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
           >
-            Let's talk
+            결제
           </button>
         </div>
       </form>
