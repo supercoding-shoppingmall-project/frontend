35358c1c49d9dc7a8c0aaacf4d726dcd42890668
aa1as <sm13929881@gmail.com>
2024-07-26 19:36:47 +0900

    [SingUp.jsx] 코드 수정 인풋창 위치 재배정

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,28 +3,10 @@ export default function SingUp() {
     <div className="space-y-12 p-20">
       <div className="border-b border-gray-900/10 pb-12">
         <h1 className="text-base font-semibold leading-7 text-gray-900">
-          프로필
+          사용하실 email 과 password를 입력해주세요
         </h1>
         <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div className="sm:col-span-4">
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              이름
-            </label>
-            <div className="mt-2">
-              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input
-                  id="username"
-                  name="username"
-                  type="text"
-                  placeholder="이름을 적어주세요..."
-                  autoComplete="username"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
             <label
               htmlFor="username"
               className="block text-sm font-medium leading-6 text-gray-900 pt-5"
@@ -73,8 +55,8 @@ export default function SingUp() {
           물건을 받으실 주소를 적어주세요
         </p>
 
-        <div className="mt-10 grid grid-cols-1 gap-x-5 gap-y-5 sm:grid-cols-6">
-          <div className="sm:col-span-3">
+        <div className="mt-10 grid grid-cols-1 gap-x-5 gap-y-5 sm:grid-cols-4">
+          <div className="sm:col-span-4">
             <label
               htmlFor="first-name"
               className="block text-sm font-medium leading-6 text-gray-900"
@@ -99,7 +81,7 @@ export default function SingUp() {
             >
               전화번호
             </label>
-            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 ">
               <input
                 id="phone"
                 name="phone"
@@ -109,9 +91,24 @@ export default function SingUp() {
               />
             </div>
           </div>
-
-          <fieldset>
-            <p class="mt-1 text-sm leading-6 text-gray-600">성별</p>
+          <div className="sm:col-span-4">
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              email
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-4">
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              성별
+            </label>
             <div class="mt-6 space-y-6">
               <div class="flex items-center gap-x-3">
                 <input
@@ -142,16 +139,16 @@ export default function SingUp() {
                 </label>
               </div>
             </div>
-          </fieldset>
+          </div>
 
-          <div className="col-span-full">
+          <div className="col-span-4">
             <label
               htmlFor="street-address"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
               주소
             </label>
-            <div className="mt-2">
+            <div className="mt-4">
               <input
                 id="street-address"
                 name="street-address"
