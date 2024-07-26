78804203a2f5dbe5c5b570aa03350a13ced7b5cc
aa1as <sm13929881@gmail.com>
2024-07-26 14:01:28 +0900

    [UserProfile.jsx] 프로필 페이지 제작

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -4,60 +4,181 @@ import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 const UserProfile = () => {
   return (
     <>
-      <div className="col-span-full">
-        <label
-          htmlFor="photo"
-          className="block text-sm font-medium leading-6 text-gray-900"
-        >
-          프로필 사진
-        </label>
-        <div className="mt-2 flex items-center gap-x-3">
-          <UserCircleIcon
-            aria-hidden="true"
-            className="h-12 w-12 text-gray-300"
-          />
-          <button
-            type="button"
-            className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-          >
-            변경
-          </button>
-        </div>
-      </div>
-      <div className="col-span-full">
-        <label
-          htmlFor="cover-photo"
-          className="block text-sm font-medium leading-6 text-gray-900"
-        >
-          파일 찾기
-        </label>
-        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-          <div className="text-center">
-            <PhotoIcon
-              aria-hidden="true"
-              className="mx-auto h-12 w-12 text-gray-300"
-            />
-            <div className="mt-4 flex text-sm leading-6 text-gray-600">
-              <label
-                htmlFor="file-upload"
-                className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-              >
-                <span>Upload a file</span>
-                <input
-                  id="file-upload"
-                  name="file-upload"
-                  type="file"
-                  className="sr-only"
+      <div className="flex justify-between mt-1.5 bg-white text-base pb-1.5 border border-gray-300">
+        <div className="w-1/3 border-t border-gray-300 py-3">
+          <div className="px-12">내 프로필</div>
+          <div className="bg-white text-center border border-gray-300 p-7 block">
+            <div>
+              <div className="mt-2 flex items-center gap-x-3">
+                <UserCircleIcon
+                  aria-hidden="true"
+                  className="h-12 w-12 text-gray-300"
                 />
-              </label>
-              <p className="pl-1">or drag and drop</p>
+                <button
+                  type="button"
+                  className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >
+                  변경
+                </button>
+              </div>
             </div>
-            <p className="text-xs leading-5 text-gray-600">
-              PNG, JPG, GIF up to 10MB
-            </p>
+          </div>
+          <div>
+            <label
+              htmlFor="cover-photo"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              파일 찾기
+            </label>
+            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+              <div className="text-center">
+                <PhotoIcon
+                  aria-hidden="true"
+                  className="mx-auto h-12 w-12 text-gray-300"
+                />
+                <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>Upload a file</span>
+                    <input
+                      id="file-upload"
+                      name="file-upload"
+                      type="file"
+                      className="sr-only"
+                    />
+                  </label>
+                  <p className="pl-1">or drag and drop</p>
+                </div>
+                <p className="text-xs leading-5 text-gray-600">
+                  PNG, JPG, GIF up to 10MB
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="text-sm font-light text-gray-700 py-1.5 px-12">
+            장바구니 상품 조회
+          </div>
+          <div className="text-sm font-light text-gray-700 py-1.5 px-12">
+            구매 목록 조회
+          </div>
+        </div>
+        <div className="w-2/3 ml-10 h-full flex flex-col border border-gray-200 p-6">
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            개인 정보
+          </h2>
+          <div className="mt-10 grid grid-cols-1 gap-x-5 gap-y-5 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                이름
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="about"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                전화번호
+              </label>
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+                  id="phone"
+                  name="phone"
+                  type="text"
+                  autoComplete="phone"
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-5">
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                email
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              성별
+            </label>
+            <div class="mt-6 space-y-6">
+              <div class="flex items-center gap-x-3">
+                <input
+                  id="push-everything"
+                  name="push-notifications"
+                  type="radio"
+                  class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                />
+                <label
+                  for="push-everything"
+                  class="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  남성
+                </label>
+              </div>
+              <div class="flex items-center gap-x-3">
+                <input
+                  id="push-email"
+                  name="push-notifications"
+                  type="radio"
+                  class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                />
+                <label
+                  for="push-email"
+                  class="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  여성
+                </label>
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="street-address"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                주소
+              </label>
+              <div className="mt-2">
+                <input
+                  id="street-address"
+                  name="street-address"
+                  type="text"
+                  autoComplete="street-address"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
+      <button
+        type="submit"
+        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        회원탈퇴
+      </button>
     </>
   );
 };
