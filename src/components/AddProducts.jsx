823acbe3250df3f41592e003fd643ea835ed3ba2
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-27 12:24:32 +0900

    [AddProducts.jsx] 취소, 상품등록 버튼 클릭시 확인 모달 띄우기

--- src/components/AddProducts.jsx
+++ src/components/AddProducts.jsx
@@ -1,143 +1,173 @@
+import { useNavigate } from "react-router-dom";
 import AddImages from "./AddImages";
 import ProductDescription from "./ProductDescription";
 import SizeQuantity from "./SizeQuantity";
+import { useState } from "react";
+import AddProductsModal from "./AddProductsModal";
 
 export default function AddProducts() {
+  const [cancelClicked, setCancelClicked] = useState(false);
+  const [addClicked, setAddClicked] = useState(false);
+  // const [productData, setProductData] = useState({});
+
+  const cancelHandle = () => {
+    setCancelClicked(true);
+  };
+
+  const addHandle = (event) => {
+    event.preventDefault();
+    setAddClicked(true);
+  };
+
   return (
-    <form className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-      <div className="space-y-12">
-        <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-2xl font-semibold leading-7 text-gray-900 text-white bg-indigo-600 rounded-md p-3">
-            상품 정보
-          </h2>
+    <>
+      <form
+        id="addProductsForm"
+        className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8"
+        onSubmit={addHandle}
+      >
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-2xl font-semibold leading-7 text-gray-900 text-white bg-indigo-600 rounded-md p-3">
+              상품 정보
+            </h2>
 
-          {/* 이미지 등록 */}
-          <AddImages />
+            {/* 이미지 등록 */}
+            <AddImages />
 
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            {/* 카테고리 선택 */}
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="size"
-                className="block font-bold leading-6 text-gray-900"
-              >
-                카테고리
-              </label>
-              <div className="mt-2">
-                <select
-                  id="category"
-                  name="category"
-                  autoComplete="category-name"
-                  className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              {/* 카테고리 선택 */}
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="size"
+                  className="block font-bold leading-6 text-gray-900"
                 >
-                  <option>샌들 & 슬리퍼</option>
-                  <option>러닝</option>
-                  <option>축구</option>
-                  <option>농구</option>
-                  <option>트레이닝 & 짐</option>
-                  <option>골프</option>
-                  <option>테니스</option>
-                  <option>기타</option>
-                </select>
-              </div>
-            </div>
-
-            {/* 상품명 */}
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="productName"
-                className="block font-bold leading-6 text-gray-900"
-              >
-                상품명
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-                  <input
-                    id="productName"
-                    name="productName"
-                    type="text"
-                    autoComplete="productName"
-                    className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
-                  />
+                  카테고리
+                </label>
+                <div className="mt-2">
+                  <select
+                    id="category"
+                    name="category"
+                    autoComplete="category-name"
+                    className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
+                  >
+                    <option>샌들 & 슬리퍼</option>
+                    <option>러닝</option>
+                    <option>축구</option>
+                    <option>농구</option>
+                    <option>트레이닝 & 짐</option>
+                    <option>골프</option>
+                    <option>테니스</option>
+                    <option>기타</option>
+                  </select>
                 </div>
               </div>
-            </div>
-          </div>
 
-          <div className="mt-10">
-            <div className="block font-bold leading-6 text-gray-900 sm:col-span-1">
-              사이즈 및 개수
-            </div>
-            <div className="mt-2 grid grid-cols-2 gap-x-6 gap-y-2 md:grid-cols-4 lg:grid-cols-8">
-              {/* 사이즈 선택 */}
-              <SizeQuantity />
-            </div>
-          </div>
-
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            {/* 가격 */}
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="price"
-                className="block font-bold leading-6 text-gray-900"
-              >
-                가격
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input
-                    id="price"
-                    name="price"
-                    type="number"
-                    autoComplete="price"
-                    className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
-                  />
+              {/* 상품명 */}
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="productName"
+                  className="block font-bold leading-6 text-gray-900"
+                >
+                  상품명
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                    <input
+                      id="productName"
+                      name="productName"
+                      type="text"
+                      autoComplete="productName"
+                      className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
 
-            {/* 날짜 */}
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="closingDate"
-                className="block font-bold leading-6 text-gray-900"
-              >
-                판매 마감
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input
-                    id="closingDate"
-                    name="closingDate"
-                    type="date"
-                    autoComplete="closingDate"
-                    className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
-                  />
+            <div className="mt-10">
+              <div className="block font-bold leading-6 text-gray-900 sm:col-span-1">
+                사이즈 및 개수
+              </div>
+              <div className="mt-2 grid grid-cols-2 gap-x-6 gap-y-2 md:grid-cols-4 lg:grid-cols-8">
+                {/* 사이즈 선택 */}
+                <SizeQuantity />
+              </div>
+            </div>
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              {/* 가격 */}
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="price"
+                  className="block font-bold leading-6 text-gray-900"
+                >
+                  가격
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      id="price"
+                      name="price"
+                      type="number"
+                      autoComplete="price"
+                      className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              {/* 날짜 */}
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="closingDate"
+                  className="block font-bold leading-6 text-gray-900"
+                >
+                  판매 마감
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      id="closingDate"
+                      name="closingDate"
+                      type="date"
+                      autoComplete="closingDate"
+                      className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <ProductDescription />
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <ProductDescription />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button
-          type="button"
-          className="text-sm font-semibold leading-6 text-gray-900"
-        >
-          취소
-        </button>
-        <button
-          type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          상품 등록
-        </button>
-      </div>
-    </form>
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button
+            type="button"
+            className="text-sm font-semibold leading-6 text-gray-900"
+            onClick={cancelHandle}
+          >
+            취소
+          </button>
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            상품 등록
+          </button>
+        </div>
+      </form>
+
+      <AddProductsModal
+        cancelClicked={cancelClicked}
+        setCancelClicked={setCancelClicked}
+        addClicked={addClicked}
+        setAddClicked={setAddClicked}
+      />
+    </>
   );
 }
