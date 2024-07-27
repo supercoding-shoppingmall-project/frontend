f588cbb92ea1e4c434e1be6c7510f68bf1082c9d
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-27 14:10:55 +0900

    [SizeQuantity.jsx] map 사용해서 리팩토링

--- src/components/SizeQuantity.jsx
+++ src/components/SizeQuantity.jsx
@@ -1,136 +1,35 @@
 import React from "react";
 
+const sizes = [
+  { id: "size240", size: "240" },
+  { id: "size250", size: "250" },
+  { id: "size260", size: "260" },
+  { id: "size270", size: "270" },
+  { id: "size280", size: "280" },
+  { id: "size290", size: "290" },
+  { id: "size300", size: "300" },
+  { id: "size310", size: "310" },
+];
+
 const SizeQuantity = () => {
   return (
     <>
-      <div className="mt-2 col-span-1">
-        <div>
-          <label htmlFor="240" className="block leading-6 text-gray-900">
-            240
+      {sizes.map((size) => (
+        <div className="mt-2 col-span-1" key={size.id}>
+          <label htmlFor={size.size} className="block leading-6 text-gray-900">
+            {size.size}
           </label>
           <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-xs">
             <input
-              id="240"
-              name="240"
+              id={size.size}
+              name={size.size}
               type="number"
-              autoComplete="240"
+              autoComplete={size.size}
               className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
             />
           </div>
         </div>
-      </div>
-      <div className="mt-2 col-span-1">
-        <div>
-          <label htmlFor="250" className="block leading-6 text-gray-900">
-            250
-          </label>
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-xs">
-            <input
-              id="250"
-              name="250"
-              type="number"
-              autoComplete="250"
-              className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="mt-2 col-span-1">
-        <div>
-          <label htmlFor="260" className="block leading-6 text-gray-900">
-            260
-          </label>
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-xs">
-            <input
-              id="260"
-              name="260"
-              type="number"
-              autoComplete="260"
-              className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="mt-2 col-span-1">
-        <div>
-          <label htmlFor="270" className="block leading-6 text-gray-900">
-            270
-          </label>
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-xs">
-            <input
-              id="270"
-              name="270"
-              type="number"
-              autoComplete="270"
-              className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="mt-2 col-span-1">
-        <div>
-          <label htmlFor="280" className="block leading-6 text-gray-900">
-            280
-          </label>
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-xs">
-            <input
-              id="280"
-              name="280"
-              type="number"
-              autoComplete="280"
-              className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="mt-2 col-span-1">
-        <div>
-          <label htmlFor="290" className="block leading-6 text-gray-900">
-            290
-          </label>
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-xs">
-            <input
-              id="290"
-              name="290"
-              type="number"
-              autoComplete="290"
-              className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="mt-2 col-span-1">
-        <div>
-          <label htmlFor="300" className="block leading-6 text-gray-900">
-            300
-          </label>
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-xs">
-            <input
-              id="300"
-              name="300"
-              type="number"
-              autoComplete="300"
-              className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="mt-2 col-span-1">
-        <div>
-          <label htmlFor="310" className="block leading-6 text-gray-900">
-            310
-          </label>
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-xs">
-            <input
-              id="310"
-              name="310"
-              type="number"
-              autoComplete="310"
-              className="block w-full flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none h-10"
-            />
-          </div>
-        </div>
-      </div>
+      ))}
     </>
   );
 };
