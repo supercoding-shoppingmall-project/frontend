fe7c709a25ddc61e1421100aca61ea0e9257b439
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-29 14:46:15 +0900

    [MyProducts.jsx] 판매리스트에서 이미지 안보이게 수정

--- src/components/myproducts/MyProducts.jsx
+++ src/components/myproducts/MyProducts.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import QuantityModal from "./QuantityModal";
 import { PRODUCTS } from "./MyData";
+import { SIZES } from "../addproduct/AddData";
 
 export default function MyProducts() {
   const [isClicked, setIsClicked] = useState(false);
@@ -17,10 +18,13 @@ export default function MyProducts() {
         <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-7xl">
           <h2 className="sr-only">Products</h2>
 
-          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 xl:gap-x-8">
             {PRODUCTS.map((product) => (
-              <div key={product.id} className="group">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7 relative">
+              <div
+                key={product.id}
+                className="group p-3 border border-solid border-slate-300 rounded"
+              >
+                {/* <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7 relative">
                   <img
                     alt={product.imageAlt}
                     src={product.imageSrc}
@@ -32,13 +36,21 @@ export default function MyProducts() {
                   >
                     재고 수량 변경
                   </button>
-                </div>
-                <h3 className="mt-4 text-lg font-bold text-gray-900">
-                  {product.name}
-                </h3>
-                <div className="mt-1 text-sm font-medium text-gray-700 flex justify-between">
-                  <div>{product.price} 원</div>
+                </div> */}
+                <div className="mt-1 text-sm font-medium text-gray-700 flex justify-between items-center">
+                  <h3 className="text-lg font-bold text-gray-900">
+                    {product.name}
+                  </h3>
                   <div>{product.closingDate}</div>
+                </div>
+                <div className="mt-1 text-sm font-medium text-gray-700 flex justify-between items-center">
+                  <div>{product.price} 원</div>
+                  <button
+                    onClick={btnClickHandle}
+                    className="text-center bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm flex justify-around rounded"
+                  >
+                    재고 수량 변경
+                  </button>
                 </div>
               </div>
             ))}
@@ -47,7 +59,11 @@ export default function MyProducts() {
       </div>
 
       {/* 재고수량 변경 모달 */}
-      <QuantityModal isClicked={isClicked} setIsClicked={setIsClicked} />
+      <QuantityModal
+        isClicked={isClicked}
+        setIsClicked={setIsClicked}
+        sizes={SIZES}
+      />
     </>
   );
 }
