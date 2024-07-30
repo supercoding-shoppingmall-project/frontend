05b173e9f36642f7c4f4f026711284fcf06101b0
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-30 22:02:31 +0900

    [App.jsx] 판매물품등록 페이지 파일로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import "./App.css";
 import MainPage from "./pages/MainPage";
 import DetailPage from "./pages/DetailPage";
 import SellingPage from "./pages/SellingPage";
+import ProductAddPage from "./pages/ProductAddPage";
 import PayPage from "./pages/PayPage";
 import MockDatas from "./components/mock-data/MockDatas";
 import Header from "./components/header-footer/Header";
@@ -13,7 +14,6 @@ import Footer from "./components/header-footer/Footer";
 import Login from "./components/login/Login";
 import SignUp from "./components/signup/SignUp";
 import UserProfile from "./components/user-profile/UserProfile";
-import AddProducts from "./components/add-product/AddProducts";
 import CartList from "./components/user-profile/CartList";
 import PurchaseList from "./components/user-profile/PurchaseList";
 import CartPage from "./components/cart/CartPage";
@@ -34,7 +34,7 @@ function App() {
           <Route path="/userprofile" element={<UserProfile />} />
           <Route path="/cartpage" element={<CartPage />} />
           <Route path="/sell" element={<SellingPage />} />
-          <Route path="/sell/add" element={<AddProducts />} />
+          <Route path="/sell/add" element={<ProductAddPage />} />
           <Route path="/pay" element={<PayPage />} />
           <Route path="/cartlist" element={<CartList />} />
           <Route path="/purchaselist" element={<PurchaseList />} />
