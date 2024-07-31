a4ffd867ec79864b6bd0706a9222652c5cd85f91
aa1as <sm13929881@gmail.com>
2024-07-31 17:40:58 +0900

    [Modal.jsx] 회원탈퇴시 이메일 입력 추가

--- src/components/user-profile/Modal.jsx
+++ src/components/user-profile/Modal.jsx
@@ -43,8 +43,18 @@ export default function Modal({ open, setOpen }) {
                   <p className="text-sm text-gray-500 mt-2 mb-2">
                     회원탈퇴시 모든정보는 지워지며 다시 되돌리수 없습니다.
                     <br />
-                    비밀번호를 입력해 주세요.
+                    이메일 / 비밀번호를 입력해 주세요.
                   </p>
+                  <div className="rounded-md ring-1 ring-gray-300  ">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="email"
+                      autoComplete="email"
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                   <div className="rounded-md ring-1 ring-gray-300  ">
                     <input
                       id="password"
