ba91d6fb8a37c5959839c0c40a10ad0bc40f31d3
Sammyissewon <person1341@gmail.com>
2024-08-01 11:38:37 +0700

    [DetailPage] 데이터 렌더링-1

--- src/components/mock-data/ImageGallery.jsx
+++ src/components/mock-data/ImageGallery.jsx
@@ -5,22 +5,22 @@ const ImageGallery = ({ imageSrc }) => {
     <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:max-w-7xl lg:grid lg:grid-cols-3 lg:gap-x-8 lg:px-8">
       <div className="lg:col-span-2 lg:row-span-2 aspect-h-4 aspect-w-3 overflow-hidden rounded-lg">
         <img
-          src={imageSrc}
-          alt={imageSrc}
+          src={imageSrc[1]}
+          alt={imageSrc[1]}
           className="h-full w-full object-cover object-center"
         />
       </div>
       <div className="lg:col-span-1 lg:row-start-1 aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
         <img
-          src={imageSrc}
-          alt={imageSrc}
+          src={imageSrc[0]}
+          alt={imageSrc[0]}
           className="h-full w-full object-cover object-center"
         />
       </div>
       <div className="lg:col-span-1 lg:row-start-2 aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
         <img
-          src={imageSrc}
-          alt={imageSrc}
+          src={imageSrc[2]}
+          alt={imageSrc[2]}
           className="h-full w-full object-cover object-center"
         />
       </div>
