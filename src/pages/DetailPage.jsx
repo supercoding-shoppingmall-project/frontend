13ea8711dca28a216ed3268f5b84ab2c0f624fb9
Sammyissewon <person1341@gmail.com>
2024-08-01 12:02:47 +0700

    [DetailPage] 데이터 렌더링-3

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -46,7 +46,7 @@ export default function DetailPage() {
   return (
     <div className="bg-white">
       <div className="pt-6">
-        <ImageGallery imageSrc={product.imageUrls[0]} />
+        <ImageGallery imageSrc={product.imageUrls} />
         <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
           <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
             <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
