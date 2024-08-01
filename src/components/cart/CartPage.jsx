274d4e67b6576bd8f76a12447e214567f4506abe
hbin3673 <hbin3673@gmail.com>
2024-08-01 17:07:35 +0900

    [장바구니] 장바구니 수정 1

--- src/components/cart/CartPage.jsx
+++ src/components/cart/CartPage.jsx
@@ -47,85 +47,182 @@ export default function CartPage({ showPurchaseButton = true }) {
     <div className="max-w-3xl mx-auto p-4">
       <h2 className="text-2xl font-bold mb-6 text-center">장바구니</h2>
 
-      <div className="mb-6">
-        <ul role="list" className="-my-6 divide-y divide-gray-200">
-          {cart.map((product) => (
-            <li key={`${product.id}-${product.size}`} className="flex py-6">
-              <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                <img
-                  alt={product.imageAlt}
-                  src={product.imageSrc}
-                  className="h-full w-full object-cover object-center"
-                />
-              </div>
-
-              <div className="ml-4 flex flex-1 flex-col">
-                <div>
-                  <div className="flex justify-between text-base font-medium text-gray-900">
-                    <h3>
-                      <a href={product.href}>{product.name}</a>
-                    </h3>
-                    <p className="ml-4">{product.price}</p>
+      {cart.length === 0 ? (
+        <p className="text-center text-gray-500">장바구니에 항목이 없습니다.</p>
+      ) : (
+        <>
+          <div className="mb-6">
+            <ul role="list" className="-my-6 divide-y divide-gray-200">
+              {cart.map((product) => (
+                <li key={`${product.id}-${product.size}`} className="flex py-6">
+                  <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                    <img
+                      alt={product.imageAlt}
+                      src={product.imageSrc}
+                      className="h-full w-full object-cover object-center"
+                    />
                   </div>
-                  <p className="mt-1 text-sm text-gray-500">{product.size}</p>
-                </div>
-                <div className="flex flex-1 items-end justify-between text-sm">
-                  <div className="flex items-center">
-                    <button
-                      onClick={(e) => {
-                        e.preventDefault();
-                        quantityChangeHandle(product.id, product.size, -1);
-                      }}
-                      className="text-gray-500 hover:text-gray-700"
-                    >
-                      <MinusIcon className="h-5 w-5" />
-                    </button>
-                    <p className="mx-2 text-gray-500">{product.quantity} 개</p>
-                    <button
-                      onClick={(e) => {
-                        e.preventDefault();
-                        quantityChangeHandle(product.id, product.size, 1);
-                      }}
-                      className="text-gray-500 hover:text-gray-700"
-                    >
-                      <PlusIcon className="h-5 w-5" />
-                    </button>
-                  </div>
-                  <div className="flex">
-                    <button
-                      type="button"
-                      onClick={() => removeFromCart(product.id, product.size)}
-                      className="font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Remove
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
 
-      <div className="border-t border-gray-200 py-6">
-        <div className="flex justify-between text-base font-medium text-gray-900">
-          <p>총 가격</p>
-          <p>{FormatToKRW(calculateTotalPrice())}</p>
-        </div>
-
-        {showPurchaseButton && (
-          <div className="mt-6 flex justify-center">
-            <Link to={"/pay"}>
-              <button
-                type="button"
-                className="flex items-center justify-center rounded-md border border-transparent bg-green-300 px-6 py-3 text-base font-semibold text-white shadow-sm hover:bg-green-400"
-              >
-                구매하기
-              </button>
-            </Link>
+                  <div className="ml-4 flex flex-1 flex-col">
+                    <div>
+                      <div className="flex justify-between text-base font-medium text-gray-900">
+                        <h3>
+                          <a href={product.href}>{product.name}</a>
+                        </h3>
+                        <p className="ml-4">{product.price}</p>
+                      </div>
+                      <p className="mt-1 text-sm text-gray-500">
+                        {product.size}
+                      </p>
+                    </div>
+                    <div className="flex flex-1 items-end justify-between text-sm">
+                      <div className="flex items-center">
+                        <button
+                          onClick={(e) => {
+                            e.preventDefault();
+                            quantityChangeHandle(product.id, product.size, -1);
+                          }}
+                          className="text-gray-500 hover:text-gray-700"
+                        >
+                          <MinusIcon className="h-5 w-5" />
+                        </button>
+                        <p className="mx-2 text-gray-500">
+                          {product.quantity} 개
+                        </p>
+                        <button
+                          onClick={(e) => {
+                            e.preventDefault();
+                            quantityChangeHandle(product.id, product.size, 1);
+                          }}
+                          className="text-gray-500 hover:text-gray-700"
+                        >
+                          <PlusIcon className="h-5 w-5" />
+                        </button>
+                      </div>
+                      <div className="flex">
+                        <button
+                          type="button"
+                          onClick={() =>
+                            removeFromCart(product.id, product.size)
+                          }
+                          className="font-medium text-indigo-600 hover:text-indigo-500"
+                        >
+                          Remove
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
-        )}
-      </div>
+          <div className="border-t border-gray-200 py-6">
+            <div className="flex justify-between text-base font-medium text-gray-900">
+              <p>총 가격</p>
+              <p>{FormatToKRW(calculateTotalPrice())}</p>
+            </div>
+            {showPurchaseButton && (
+              <div className="mt-6 flex justify-center">
+                <Link to={"/pay"}>
+                  <button
+                    type="button"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-green-300 px-6 py-3 text-base font-semibold text-white shadow-sm hover:bg-green-400"
+                  >
+                    구매하기
+                  </button>
+                </Link>
+              </div>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
+
+//   return (
+//     <div className="max-w-3xl mx-auto p-4">
+//       <h2 className="text-2xl font-bold mb-6 text-center">장바구니</h2>
+
+//       <div className="mb-6">
+//         <ul role="list" className="-my-6 divide-y divide-gray-200">
+//           {cart.map((product) => (
+//             <li key={`${product.id}-${product.size}`} className="flex py-6">
+//               <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+//                 <img
+//                   alt={product.imageAlt}
+//                   src={product.imageSrc}
+//                   className="h-full w-full object-cover object-center"
+//                 />
+//               </div>
+
+//               <div className="ml-4 flex flex-1 flex-col">
+//                 <div>
+//                   <div className="flex justify-between text-base font-medium text-gray-900">
+//                     <h3>
+//                       <a href={product.href}>{product.name}</a>
+//                     </h3>
+//                     <p className="ml-4">{product.price}</p>
+//                   </div>
+//                   <p className="mt-1 text-sm text-gray-500">{product.size}</p>
+//                 </div>
+//                 <div className="flex flex-1 items-end justify-between text-sm">
+//                   <div className="flex items-center">
+//                     <button
+//                       onClick={(e) => {
+//                         e.preventDefault();
+//                         quantityChangeHandle(product.id, product.size, -1);
+//                       }}
+//                       className="text-gray-500 hover:text-gray-700"
+//                     >
+//                       <MinusIcon className="h-5 w-5" />
+//                     </button>
+//                     <p className="mx-2 text-gray-500">{product.quantity} 개</p>
+//                     <button
+//                       onClick={(e) => {
+//                         e.preventDefault();
+//                         quantityChangeHandle(product.id, product.size, 1);
+//                       }}
+//                       className="text-gray-500 hover:text-gray-700"
+//                     >
+//                       <PlusIcon className="h-5 w-5" />
+//                     </button>
+//                   </div>
+//                   <div className="flex">
+//                     <button
+//                       type="button"
+//                       onClick={() => removeFromCart(product.id, product.size)}
+//                       className="font-medium text-indigo-600 hover:text-indigo-500"
+//                     >
+//                       Remove
+//                     </button>
+//                   </div>
+//                 </div>
+//               </div>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+
+//       <div className="border-t border-gray-200 py-6">
+//         <div className="flex justify-between text-base font-medium text-gray-900">
+//           <p>총 가격</p>
+//           <p>{FormatToKRW(calculateTotalPrice())}</p>
+//         </div>
+
+//         {showPurchaseButton && (
+//           <div className="mt-6 flex justify-center">
+//             <Link to={"/pay"}>
+//               <button
+//                 type="button"
+//                 className="flex items-center justify-center rounded-md border border-transparent bg-green-300 px-6 py-3 text-base font-semibold text-white shadow-sm hover:bg-green-400"
+//               >
+//                 구매하기
+//               </button>
+//             </Link>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
