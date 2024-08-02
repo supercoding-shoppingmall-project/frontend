9d961d596acb96903d5e27f424cb742b62192b95
hbin3673 <hbin3673@gmail.com>
2024-08-02 20:30:45 +0900

    주석 제거

--- src/utils/ApiService.jsx
+++ src/utils/ApiService.jsx
@@ -1,27 +1,3 @@
-// // apiService.js
-
-// import axios from "axios";
-
-// const API_BASE_URL = "/api"; // API의 기본 URL 설정
-
-// // DELETE 요청을 처리하는 함수
-// export const deleteCartItem = async (id, userId) => {
-//   const token = localStorage.getItem("Authorization");
-//   try {
-//     await axios.delete(`${API_BASE_URL}/cart/${userId}/items/${id}`, {
-//       headers: {
-//         Authorization: token,
-//       },
-//       // data: { productId, size },
-//     });
-//     window.location.reload();
-//   } catch (error) {
-//     console.error("Failed to remove item from cart", error);
-//     throw error;
-//   }
-// };
-// apiService.js
-
 import axios from "axios";
 
 const API_BASE_URL = "/api"; // API의 기본 URL 설정
