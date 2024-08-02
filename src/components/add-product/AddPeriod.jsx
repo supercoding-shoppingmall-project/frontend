554fd5ab20d540d70a9080710acd7d7c6fe2ccea
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-02 14:40:07 +0900

    [AddPeriod.jsx] 등록날짜 입력창 삭제

--- src/components/add-product/AddPeriod.jsx
+++ src/components/add-product/AddPeriod.jsx
@@ -3,6 +3,7 @@ import { useInputValue } from "../../hooks/useInputValue";
 
 const AddPeriod = () => {
   const [endtime, setendtime] = useInputValue("");
+  const today = new Date().toISOString().split("T")[0];
 
   return (
     <div className="sm:col-span-2">
