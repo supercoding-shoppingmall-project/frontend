93e20d81ca4da30ad0dcfdbdaca3ede95096491c
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-03 14:15:04 +0900

    [QuantityModal.jsx] 재고수량 업데이트 시 데이터 형식 변경

--- src/components/my-products/QuantityModal.jsx
+++ src/components/my-products/QuantityModal.jsx
@@ -45,8 +45,16 @@ export default function QuantityModal({
   };
 
   const submitHandle = async (event) => {
-    event.preventDefault(); // 기본 폼 제출 방지
-    console.log("전송할 productName:", productName); // productName 로그 추가
+    event.preventDefault();
+    console.log("전송할 productName:", productName);
+
+    const updatedStockDtos = Object.entries(quantityData).map(
+      ([size, sizeStock]) => ({
+        size: Number(size),
+        sizeStock: sizeStock,
+      })
+    );
+
     try {
       const token = localStorage.getItem("Authorization");
       const response = await axios.put(
@@ -58,7 +66,6 @@ export default function QuantityModal({
           },
         }
       );
-      // 성공적으로 업데이트된 후 모달 닫기
       if (response.status === 200) {
         setOpen(false);
         setIsClicked(false);
