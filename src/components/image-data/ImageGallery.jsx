94a68282d6cf7e1ad9c14c9689d6b43dd4bdcb2c
Sammyissewon <person1341@gmail.com>
2024-08-04 22:55:55 +0700

    [ImageGallery] 리팩토링

--- src/components/image-data/ImageGallery.jsx
+++ src/components/image-data/ImageGallery.jsx
@@ -1,35 +1,26 @@
 import React from "react";
 
 const ImageGallery = ({ imageSrc = [] }) => {
+  const renderImage = (src, index) => (
+    <div
+      key={index}
+      className={`lg:col-span-${index === 1 ? "2" : "1"} ${
+        index === 1 ? "lg:row-span-2" : "lg:row-span-1"
+      } aspect-h-${
+        index === 1 ? "4" : "2"
+      } aspect-w-3 overflow-hidden rounded-lg`}
+    >
+      <img
+        src={src}
+        alt={`Image ${src}`}
+        className="h-full w-full object-cover object-center"
+      />
+    </div>
+  );
+
   return (
     <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:max-w-7xl lg:grid lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-      {imageSrc[1] && (
-        <div className="lg:col-span-2 lg:row-span-2 aspect-h-4 aspect-w-3 overflow-hidden rounded-lg">
-          <img
-            src={imageSrc[1]}
-            alt={`Image ${imageSrc[1]}`}
-            className="h-full w-full object-cover object-center"
-          />
-        </div>
-      )}
-      {imageSrc[0] && (
-        <div className="lg:col-span-1 lg:row-start-1 aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-          <img
-            src={imageSrc[0]}
-            alt={`Image ${imageSrc[0]}`}
-            className="h-full w-full object-cover object-center"
-          />
-        </div>
-      )}
-      {imageSrc[2] && (
-        <div className="lg:col-span-1 lg:row-start-2 aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-          <img
-            src={imageSrc[2]}
-            alt={`Image ${imageSrc[2]}`}
-            className="h-full w-full object-cover object-center"
-          />
-        </div>
-      )}
+      {imageSrc.map((src, index) => index < 3 && renderImage(src, index))}
     </div>
   );
 };
