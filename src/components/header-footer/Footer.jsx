bc05a5c268a209ca3828acc670c742a3246e2d64
hbin3673 <hbin3673@gmail.com>
2024-08-05 14:24:23 +0900

    [푸터] 마진 삭제

--- src/components/header-footer/Footer.jsx
+++ src/components/header-footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-green-500 text-white content-end items-end">
+    <footer className="bg-green-500 text-white content-end">
       <div className="container mx-auto px-4 py-8">
         <div className="flex flex-wrap justify-between items-center">
           <div className="w-full lg:w-1/3 mb-6 lg:mb-0">
