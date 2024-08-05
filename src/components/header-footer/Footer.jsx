b5462ec085015faa23471c17c8b77b08d1538cc5
hbin3673 <hbin3673@gmail.com>
2024-08-05 14:08:32 +0900

    [푸터] 마진 추가

--- src/components/header-footer/Footer.jsx
+++ src/components/header-footer/Footer.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-green-500 text-white content-end mt-36">
+    <footer className="bg-green-500 text-white content-end mt-45">
       <div className="container mx-auto px-4 py-8">
         <div className="flex flex-wrap justify-between items-center">
           <div className="w-full lg:w-1/3 mb-6 lg:mb-0">
             <h5 className="text-lg font-semibold mb-4">Source</h5>
-            <p className="text-sm text-white-400">
+            <p className="text-sm text-white-700">
               모든 상품의 가격 및 이미지의 출처는 다음과 같습니다.
               <br />
               https://www.nike.com/kr/ (한국 나이키 웹사이트)
