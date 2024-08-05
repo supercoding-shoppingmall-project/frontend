ef70ca8d90fb24f7fde7285b5c0a2f5a2e6a8d93
hbin3673 <hbin3673@gmail.com>
2024-08-05 14:45:50 +0900

    [장바구니 목록] 장바구니 목록 div 태그 주석처리

--- src/components/user-profile/CartList.jsx
+++ src/components/user-profile/CartList.jsx
@@ -7,9 +7,9 @@ const CartList = () => {
     <>
       <div className="flex flex-col sm:flex-row justify-center bg-white text-base py-40 px-10 sm:px-80 border border-gray-300">
         <User />
-        <div className="sm:w-2/3 sm:ml-10 w-full h-full flex flex-col border border-solid border-gray-200 p-6">
+        {/* <div className="sm:w-2/3 sm:ml-10 w-full h-full flex flex-col border border-solid border-gray-200 p-6">
           장바구니 목록
-        </div>
+        </div> */}
         <div>
           <CartPage />
         </div>
