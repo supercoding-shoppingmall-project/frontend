3243f4ab51bcb9b2747d4933be694620a9b4b19e
hbin3673 <hbin3673@gmail.com>
2024-08-05 15:53:45 +0900

    [ApiService] 원래로 되돌림

--- src/utils/ApiService.jsx
+++ src/utils/ApiService.jsx
@@ -3,15 +3,16 @@ import axios from "axios";
 const API_BASE_URL = "/api"; // API의 기본 URL 설정
 
 // DELETE 요청을 처리하는 함수
-export const deleteCartItem = async (productId, size, userId) => {
+export const deleteCartItem = async (id, userId) => {
   const token = localStorage.getItem("Authorization");
-  await axios.delete(`/api/cart/${userId}`, {
-    headers: {
-      Authorization: token,
-    },
-    data: {
-      productId,
-      size,
-    },
-  });
+  try {
+    await axios.delete(`${API_BASE_URL}/cart/${userId}/items/${id}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  } catch (error) {
+    console.error("Failed to remove item from cart", error);
+    throw error;
+  }
 };
