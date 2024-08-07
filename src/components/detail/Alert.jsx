2f173a4366b95b67908a5852aa5f4ca4f9f2fee2
Sammyissewon <person1341@gmail.com>
2024-08-07 10:13:23 +0700

    [ProductOption] Alert  코드수정

--- src/components/detail/Alert.jsx
+++ src/components/detail/Alert.jsx
@@ -70,7 +70,7 @@ export default function Alert({ onClose }) {
               <Link to="/cartpage">
                 <button
                   type="button"
-                  onClick={() => closeHandle(false)}
+                  onClick={closeHandle}
                   className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
                 >
                   장바구니로 이동
@@ -79,7 +79,7 @@ export default function Alert({ onClose }) {
               <button
                 type="button"
                 data-autofocus
-                onClick={() => closeHandle(false)}
+                onClick={closeHandle}
                 className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
               >
                 닫기
